1e7114be9144682aab7d5e7947ac86b52e69400d
Niklas Ferling <niklas.ferling@gmail.com>
2024-08-27 20:19:21 +0200

    Register function (not finished)

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,8 +1,10 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { useState } from "react";
 
 const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
   email: yup
     .string()
     .email("Must be a valid email address")
@@ -17,10 +19,6 @@ const schema = yup.object().shape({
     .oneOf([yup.ref("password"), null], "Passwords must match"),
 });
 
-function onRegister(data) {
-  console.log(data);
-}
-
 function RegisterForm() {
   const {
     register,
@@ -30,8 +28,54 @@ function RegisterForm() {
     resolver: yupResolver(schema),
   });
 
+  const [error, setError] = useState(null);
+
+  function onRegister({ name, email, password }) {
+    async function register(name, email, password) {
+      console.log("name, email, password");
+      try {
+        const response = await fetch(
+          "https://v2.api.noroff.dev/auth/register",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name: name,
+              email: email,
+              password: password,
+            }),
+          }
+        );
+        const data = await response.json();
+        if (!data.ok) {
+          throw new Error(data.errors[0].message);
+        } else if (data.ok) {
+          console.log(data);
+        }
+      } catch (err) {
+        setError(err);
+      }
+    }
+    register(name, email, password);
+  }
+
   return (
     <form onSubmit={handleSubmit(onRegister)} className="flex flex-col">
+      {error ? (
+        <p className="bg-red-100 border border-red-400 rounded-md p-2">
+          {error.message}
+        </p>
+      ) : null}
+      <label htmlFor="name" className="mt-3">
+        Name
+      </label>
+      <input
+        {...register("name")}
+        placeholder="Name"
+        className="focus:outline-none px-2 py-1 rounded-lg border border-green-200"
+      />
       <label htmlFor="email" className="mt-3">
         Email
       </label>
