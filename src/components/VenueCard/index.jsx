2f03ce5c511c523c1129240e4018ef6f21e158a9
Niklas Ferling <niklas.ferling@gmail.com>
2024-08-29 16:19:52 +0200

    css

--- src/components/VenueCard/index.jsx
+++ src/components/VenueCard/index.jsx
@@ -6,13 +6,13 @@ function VenueCard({ venue }) {
   }
 
   return (
-    <div className="flex-col w-40 md:w-72 overflow-hidden border rounded-xl mb-10 font-semibold">
+    <div className="flex-col w-40 md:w-72 overflow-hidden border rounded-xl mb-10 font-semibold bg-white drop-shadow-3xl hover:scale-105 transition-transform">
       <img
         src={venue.media[0].url}
         alt={venue.media[0].alt}
         className="w-full h-56 md:h-96 object-cover"
       />
-      <div className="p-3 flex-col">
+      <div className="p-3 flex-col bg-white">
         <span className="flex justify-between">
           <h3 className="text-md">{truncateText(venue.name)}</h3>
           <span className="flex gap-1 ml-4 items-center text-sm">
