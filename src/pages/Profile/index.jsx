eb1d7707bfc329342dc784dfe0cf3f184fb18fe9
Niklas Ferling <niklas.ferling@gmail.com>
2024-08-29 23:03:21 +0200

    unused import

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { logout } from "../../api/auth/logout";
-import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/authContext";
 
