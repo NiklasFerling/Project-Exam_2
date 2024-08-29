3d1f42ee850ef2e76467123dd180d05a38ad0616
Niklas Ferling <niklas.ferling@gmail.com>
2024-08-29 15:47:14 +0200

    Better venuecard fitting

--- src/components/VenueCard/index.jsx
+++ src/components/VenueCard/index.jsx
@@ -6,21 +6,21 @@ function VenueCard({ venue }) {
   }
 
   return (
-    <div className="flex-col w-72 overflow-hidden border rounded-xl mb-10 font-semibold">
+    <div className="flex-col w-40 md:w-72 overflow-hidden border rounded-xl mb-10 font-semibold">
       <img
         src={venue.media[0].url}
         alt={venue.media[0].alt}
-        className="w-full h-96 object-cover"
+        className="w-full h-56 md:h-96 object-cover"
       />
       <div className="p-3 flex-col">
         <span className="flex justify-between">
-          <h3 className="text-lg">{truncateText(venue.name)}</h3>
-          <span className="flex gap-1 ml-4 items-center">
+          <h3 className="text-md">{truncateText(venue.name)}</h3>
+          <span className="flex gap-1 ml-4 items-center text-sm">
             <i className="fa-solid fa-star"></i>
             <p>{venue.rating}</p>
           </span>
         </span>
-        <p className="text-neutral-600">
+        <p className="text-neutral-600 text-sm">
           <i className="fa-solid fa-location-dot mr-1"></i>
           {venue.location.city}
         </p>
