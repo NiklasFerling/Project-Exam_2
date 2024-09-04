e2a376d404698735218c105a965b72f06d7292ac
Niklas Ferling <niklas.ferling@gmail.com>
2024-09-04 19:06:07 +0200

    Create booking func, temporaty user feedback on success and fail

---
+++ src/api/bookings/create.jsx
@@ -0,0 +1,25 @@
+import { load } from "../../storage/load";
+
+export async function createBooking(data) {
+  const profile = load("profile");
+  const apiKey = load("API_KEY");
+  try {
+    const response = await fetch(
+      "https://v2.api.noroff.dev/holidaze/bookings",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${profile.accessToken}`,
+          "X-Noroff-API-Key": apiKey,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/pages/Venue/index.jsx
+++ src/pages/Venue/index.jsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import MyCalendar from "../../components/ReactCalendar";
+import { createBooking } from "../../api/bookings/create";
+
+const id = window.location.search.replace("?", "");
 
 const schema = yup.object().shape({
   guests: yup.number().required("Guests is required"),
@@ -18,7 +21,13 @@ const Venue = () => {
   } = useForm({ resolver: yupResolver(schema) });
 
   function onBook(data) {
-    console.log(data);
+    createBooking({ ...data, venueId: id }).then((json) => {
+      if (json.status === 200) {
+        alert("Booking Successful");
+      } else {
+        alert("Booking Failed: " + json.status);
+      }
+    });
   }
 
   const [venue, setVenue] = useState(null);
@@ -29,7 +38,6 @@ const Venue = () => {
     async function idFetch() {
       try {
         setLoading(true);
-        const id = window.location.search.replace("?", "");
         const response = await fetch(
           `https://v2.api.noroff.dev/holidaze/venues/${id}?_owner=true&_bookings=true`
         );
@@ -55,13 +63,13 @@ const Venue = () => {
 
   return (
     <div className="min-h-screen mb-20">
-      <div className="flex flex-col justify-center gap-5 mt-20  w-fit mx-auto ">
+      <div className="flex flex-col lg:flex-row justify-center gap-5 mt-10 mx-auto md:w-2/3 xl:max-w-2/3 xl:px-0">
         <img
           src={venue.media[0].url}
           alt={venue.media[0].alt}
-          className="bg-green-300 md:w-96 object-cover"
+          className="bg-green-300 object-cover max-h-96 lg-max-h-full lg:aspect-square"
         />
-        <div className="p-5 mx-auto flex flex-col md:flex-row gap-5">
+        <div className="p-5 mx-10 md:mx-0 flex flex-col gap-5 justify-center">
           <div>
             <h1 className="text-3xl mb-4">{venue.name}</h1>
             <span className="flex mb-5 items-center">
@@ -71,35 +79,50 @@ const Venue = () => {
                 {venue.location.city}, {venue.location.country}
               </p>
             </span>
-            <span className="flex mb-5 gap-3">
+            <span className="flex mb-10 gap-3">
               <p className="text-xl text-center">{venue.price}kr/night</p>|
               <p className="text-base">Max Capacity: 10</p>
             </span>
             <form
-              className="mb-5 justify-center  mt-5"
+              className="my-5 justify-center  mt-5"
               onSubmit={handleSubmit(onBook)}
             >
-              <span className="flex mb-6 gap-2 sm:flex-row justify-center items-center mb-2">
-                <input
-                  {...register("dateFrom")}
-                  type="date"
-                  className="border border-black rounded-xl p-1"
-                />
-                <p className="align-middle">to</p>
-                <input
-                  {...register("dateTo")}
-                  type="date"
-                  className="border border-black rounded-xl p-1"
-                />
+              <span className="flex mb-6 gap-2 sm:flex-row justify-center items-center">
+                <div className="rounded-xl py-2 px-4 drop-shadow-xl bg-white">
+                  <label htmlFor="dateFrom" className="mr-2">
+                    from:
+                  </label>
+                  <input
+                    {...register("dateFrom")}
+                    type="date"
+                    className="focus:outline-none"
+                  />
+                </div>
+                <p className="align-middle">-</p>
+                <div className="rounded-xl py-2 px-4 drop-shadow-xl bg-white">
+                  <label htmlFor="dateTo" className="mr-2">
+                    to:
+                  </label>
+                  <input
+                    {...register("dateTo")}
+                    type="date"
+                    className="focus:outline-none"
+                  />
+                </div>
               </span>
               <span className="flex justify-center mb-2 gap-5">
-                <input
-                  type="number"
-                  {...register("guests")}
-                  className="border border-black rounded-xl p-1 w-10 text-center"
-                />
-                <button className="border border-black rounded-xl p-1">
-                  Book
+                <div className="rounded-xl py-2 px-4 drop-shadow-xl bg-white">
+                  <label htmlFor="guests" className="mr-2">
+                    guests:
+                  </label>
+                  <input
+                    type="number"
+                    {...register("guests")}
+                    className="w-10 focus:outline-none"
+                  />
+                </div>
+                <button className="rounded-xl py-2 px-6 drop-shadow-xl bg-teal-500 text-white">
+                  Book Now
                 </button>
               </span>
             </form>
@@ -108,6 +131,8 @@ const Venue = () => {
             <span className="flex gap-2 items-center justify-center mb-2">
               <div className="w-4 h-4 rounded-md bg-red-400"></div>
               <p>Not Available</p>
+              <div className="w-4 h-4 rounded-md bg-green-300 ml-3"></div>
+              <p>Today</p>
             </span>
             <MyCalendar id={venue.id} />
           </div>
